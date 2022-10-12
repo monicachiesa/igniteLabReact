d8c9c7811a8e2f02e1c413a5e22fe08e950f906f
Mônica Chiesa <monica.chiesa@universo.univates.br>
2022-10-12 00:02:12 -0300

    first commit

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,35 @@
+import { Text, TextProps } from "./Text";
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+    title: 'Components/Text',
+    component: Text,
+    args: {
+        children: 'Lorem ipsun'
+    }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {
+
+}
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
+}
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p>Testando</p>
+        )
+    }
+}
