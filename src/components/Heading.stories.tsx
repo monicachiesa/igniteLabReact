bc8405b2bccb0219ddeb469416b547b96d31e5c1
Mônica Chiesa <monica.chiesa@universo.univates.br>
2022-10-12 07:29:55 -0300

    aula 2 ok

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -1,9 +1,9 @@
-import { Text, TextProps } from "./Text";
+import { Heading, HeadingProps } from "./Heading";
 import { Meta, StoryObj } from '@storybook/react'
 
 export default {
-    title: 'Components/Text',
-    component: Text,
+    title: 'Components/Heading',
+    component: Heading,
     args: {
         children: 'Lorem ipsun',
         size: 'md',
@@ -16,29 +16,29 @@ export default {
             }
         }
     }
-} as Meta<TextProps>
+} as Meta<HeadingProps>
 
-export const Default: StoryObj<TextProps> = {
+export const Default: StoryObj<HeadingProps> = {
 
 }
 
-export const Small: StoryObj<TextProps> = {
+export const Small: StoryObj<HeadingProps> = {
     args: {
         size: 'sm'
     }
 }
 
-export const Large: StoryObj<TextProps> = {
+export const Large: StoryObj<HeadingProps> = {
     args: {
         size: 'lg'
     }
 }
 
-export const CustomComponent: StoryObj<TextProps> = {
+export const CustomComponent: StoryObj<HeadingProps> = {
     args: {
         asChild: true,
         children: (
-            <p>Text com P</p>
+            <h1>Heading com H1</h1>
         )
     },
     argTypes: {
